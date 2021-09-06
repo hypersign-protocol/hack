3eba9bace89c875696880908d3423ebcb8122025
Mitrasish Mukherjee <34746546+rekpero@users.noreply.github.com>
2021-09-06 17:18:12 +0530

    update vue config for relative asset fix

--- solana/frontend/vue.config.js
+++ solana/frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+    ? './'
+    : './',
   chainWebpack: config => {
       config.plugins.delete("prefetch");
     },
